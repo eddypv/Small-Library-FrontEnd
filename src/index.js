1dd52f5c905cf2aee423707bdb074b81b2f1b970
Eddy Perez <eddypv0@gmail.com>
2021-12-03 16:13:33 -0400

    Init

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import {ApolloProvider, ApolloClient, HttpLink, InMemoryCache} from '@apollo/client'
+const client = new ApolloClient({
+    link:new HttpLink({
+        uri:'http://localhost:4000/'
+    }),
+    cache: new InMemoryCache()
+});
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
+,
+ document.getElementById('root'))
